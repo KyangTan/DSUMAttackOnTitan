9f8567c6e38bc9d44f7dcf6d241234fbbdbbbdc3
KyangTan <kwanyang29@gmail.com>
2022-05-25 11:43:31 +0800

    All completed

--- src/main/java/com/magiconch/backend/AOTBruh.java
+++ src/main/java/com/magiconch/backend/AOTBruh.java
@@ -25,22 +25,22 @@ public class AOTBruh {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException, ParseException, IOException, JSONException {
-        LinkedList<Member> characterList = new LinkedList<>();
+        LinkedList<Member> memberList = new LinkedList<>();
         LinkedListNode tempNode = new LinkedListNode();
-        characterList.add(new Member("Reiner Braun", 185,95,9,7,7,10,8));
-        characterList.add(new Member("Armin Arlert", 163,55,2,6,10,8,8));
-        characterList.add(new Member("Annie Leonhart", 153,54,10,7,7,3,1));
-        characterList.add(new Member("Bertholdt Hoover", 192,81,9,4,6,9,2));
-        characterList.add(new Member("Jean Kristein", 175,65,9,8,7,5,9));
-        characterList.add(new Member("Sasha Blouse", 165,55,6,3,5,6,7));
-        characterList.add(new Member("Connie Springer", 158,58,6,7,3,7,5));
-        characterList.add(new Member("Mikasa Ackerman", 170,68,10,9,8,6,7));
-        characterList.add(new Member("Eren Yeager", 170,63,9,10,3,5,10));
-        characterList.add(new Member("Historia Reiss", 145,42,4,8,7,6,5));
-        characterList.add(new Member("Levi Ackerman", 160,65,12,12,7,8,8));
-        characterList.add(new Member("Erwin Smith", 188,92,8,8,11,10,12));
-        characterList.add(new Member("Hange Zoë", 170,60,9,9,12,7,11));
-        tempNode = characterList.getHead();
+        memberList.add(new Member("Reiner Braun", 185,95,9,7,7,10,8));
+        memberList.add(new Member("Armin Arlert", 163,55,2,6,10,8,8));
+        memberList.add(new Member("Annie Leonhart", 153,54,10,7,7,3,1));
+        memberList.add(new Member("Bertholdt Hoover", 192,81,9,4,6,9,2));
+        memberList.add(new Member("Jean Kristein", 175,65,9,8,7,5,9));
+        memberList.add(new Member("Sasha Blouse", 165,55,6,3,5,6,7));
+        memberList.add(new Member("Connie Springer", 158,58,6,7,3,7,5));
+        memberList.add(new Member("Mikasa Ackerman", 170,68,10,9,8,6,7));
+        memberList.add(new Member("Eren Yeager", 170,63,9,10,3,5,10));
+        memberList.add(new Member("Historia Reiss", 145,42,4,8,7,6,5));
+        memberList.add(new Member("Levi Ackerman", 160,65,12,12,7,8,8));
+        memberList.add(new Member("Erwin Smith", 188,92,8,8,11,10,12));
+        memberList.add(new Member("Hange Zoë", 170,60,9,9,12,7,11));
+        tempNode = memberList.getHead();
 //        
 //        characterList.printList();
         // TODO code application logic here
