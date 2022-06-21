ea20c20283a127d9c38bf398f8324c87d9058d14
KyangTan <kwanyang29@gmail.com>
2022-06-22 03:42:56 +0800

    search

--- src/main/java/com/magiconch/backend/NineTitan.java
+++ src/main/java/com/magiconch/backend/NineTitan.java
@@ -32,7 +32,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(2);
                     setRunningSpeed(100);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/founding_titan.jpg");
                     setTitanDesc("The Founding Titan (始祖の巨人 Shiso no Kyojin?, also translated as Progenitor Titan) was one of the Nine Titans and the first of all Titans. Its Scream (叫び Sakebi?) could create and control other Titans and modify the memories and body compositions of the Subjects of Ymir, but this power had historically only been able to be used by members of the royal family. According to Marley's Titan Biology Research Society, the Founding Titan was the point where the paths connecting all Subjects of Ymir and Titans cross. Because of this, those who hail from Marley sometimes called it the 'Coordinate' (座標 Zahyō?).");
                     break;
                 case "Armored Titan":
@@ -44,7 +44,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(2);
                     setRunningSpeed(100);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/armored_titan.png");
                     setTitanDesc("The Armored Titan (鎧の巨人 Yoroi no Kyojin?) was one of the Nine Titans that possessed armored plates of skin across its body.");
                     break;
                 case "Attack Titan":
@@ -56,7 +56,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(2);
                     setRunningSpeed(100);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/attack_titan.jpg");
                     setTitanDesc("The Attack Titan (進撃の巨人 Shingeki no Kyojin?, also translated as Attacking Titan) was one of the Nine Titans that could see through the memories of its past and future holders, and is said to have fought for freedom throughout the course of numerous generations.");
                     break;
                 case "Beast Titan":
@@ -68,7 +68,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(2);
                     setRunningSpeed(20);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/beast_titan.jpg");
                     setTitanDesc("The Beast Titan (獣の巨人 Kemono no Kyojin?) was one of the Nine Titans. Typically, it was slightly larger than most Titans and took on an animal-like appearance.");
                     break;
                 case "Cart Titan":
@@ -80,11 +80,11 @@ public class NineTitan extends Titan {
                     setWalkingLegs(4);
                     setRunningSpeed(140);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/cart_titan.jpg");
                     setTitanDesc("");
                     break;
-                case "Colossus Titan":
-                    setName("Colossus Titan");
+                case "Colossal Titan":
+                    setName("Colossal Titan");
                     setTitantype(Titan.type.NineTitan);
                     setWp(WalkingPattern.Not_Repeated);
                     setClimb(false);
@@ -92,7 +92,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(2);
                     setRunningSpeed(50);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/colossal.png");
                     setTitanDesc("The Colossus Titan (超大型巨人 Chō ōgata Kyojin?) was one of the Nine Titans and the early main antagonist of the Attack on Titan series. It was notable for its incredible size and significant control over the steam emitted by its Titan body along with the user being able to successfully control its transformation's power.");
                     break;
                 case "Female Titan":
@@ -116,7 +116,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(4);
                     setRunningSpeed(100);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/jaw_titan.jpg");
                     setTitanDesc("The Jaw Titan (顎の巨人 Agito no Kyojin?) was one of the Nine Titans with a ferociously powerful set of jaws and claws that were able to tear through almost anything. Due to its small size, it was also known to be the fastest out of the Nine Titans. The Jaw Titan is an assault unit. Its small size makes it the swiftest of all. Its powerful claws and jaws can crush just about anything.");
 
                     break;
@@ -129,7 +129,7 @@ public class NineTitan extends Titan {
                     setWalkingLegs(2);
                     setRunningSpeed(100);
                     setDangerRisk(19);
-                    setTitanImageUrl("assets/images/female.png");
+                    setTitanImageUrl("assets/images/warHammer.png");
                     setTitanDesc("The War Hammer Titan (戦鎚の巨人 Sentsui no Kyojin?) was one of the Nine Titans, and it possessed the ability to create structures out of hardened Titan flesh. It controls Titan hardening to create weapons, or anything it needs, from underground.");
                     break;
                 default:
